624875e490f7674289795b1ea1e541bccac42969
aidanjacobson <survivalking4@gmail.com>
2024-09-07 15:36:40 -0700

    modify supervisor_utils

---
+++ root_fs/supervisor_utils.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const remote = require("./remote");
+
+const addon_slug = "local_lightcontrol";
+
+function requestAsync(options) {
+    return new Promise((resolve, reject) => {
+        request.post(options, ()=>{
+            resolve()
+        });
+    })
+}
+
+async function rebuild() {
+    if (! remote.isRemote()) {
+        console.log("Cannot rebuild because not in remote container");
+        return {status: "failure", reason: "Cannot rebuild because not in remote container"}
+    }
+    var options = {
+        'method': 'POST',
+        'url': `http://supervisor/addons/${addon_slug}/rebuild`,
+        'headers': {
+            'Authorization': `Bearer ${process.env.SUPERVISOR_TOKEN}`,
+            'Content-Type': 'application/json',
+        }
+    };
+    return {
+        token: process.env.SUPERVISOR_TOKEN,
+        response: await requestAsync(options)
+    };
+}
+
+module.exports = {rebuild};
